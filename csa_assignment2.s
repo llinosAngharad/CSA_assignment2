746c9368ac4a3c5f35ae3ff8ff3134e551a8dd1a
Llinos <llins1@hotmail.com>
2018-11-23 15:59:20 +0000

    Update csa_assignment2.s

--- csa_assignment2.s
+++ csa_assignment2.s
@@ -1,18 +1,20 @@
 	.data
 	.align 2
 
-msg:    .asciiz "\nProvide an integer for the Fibonacci computation:\n"
-comma:  .asciiz ","
+init_msg:   .asciiz "\nProvide an integer for the Fibonacci computation:\n"
+final_msg:  .asciiz "\nThe Fibonacci numbers are:\n"
+comma:      .asciiz ","
 error_msg:	.asciiz "\nInput error - must be a positive integer.\n"
 	
     .text
 main:
     li $t9, 4
     li $t8, 0
+    li $t7, 1
 
 INPUT:
 # Print message:
-    la $a0, msg     # Load address of message into $a0     
+    la $a0, init_msg     # Load address of message into $a0     
 	li $v0, 4       # System call code 4: print string
     syscall			
 # Read input N:
@@ -42,8 +44,14 @@ JFIB:
     add $a1, $a1, $t1   # Move address to end address of memo on heap
     jal fib             # Input is valid. Call fib subroutine
     
-PRINT:
-    move $a0, $v0
+PRINT_MSG:
+    move $t0, $v0
+    la $a0, final_msg   # Load address of final message into $a0 to be printed
+    li $v0, 4           # System call code 4: print string
+    syscall
+    
+PRINT_FIB:
+    move $a0, $t0   # Load integer returned from fib into $a0 to be printed
     li $v0, 1       # Print integer
     syscall
     
@@ -81,7 +89,7 @@ MAKE_HEAP_EXIT:
 # Returns all fibonacci numbers from 0 to n
 #
 # @param $a0: user input N
-# @param $a1: end address of array on heap
+# @param $a1: end address of array on heap i.e address of memo[n]
 #
 # @return $v0: fibonacci number
 ##
@@ -90,5 +98,63 @@ fib:
     sw $ra, 8($sp)
     sw $a0, 4($sp)      # Parameter $a0
     sw $a1, 0($sp)      # Parameter $a1
-    move $v0, $a0
+    
+    move $t0, $a0       #  
+    move $t1, $a1       # Address of memo[n]
+    
+    
+    blez $t0, ZERO      # if N <= 0, fib(0)=0, jump to ZERO
+    beq $t0, $t7, FIB1  # else if N == 1, fib(1)=1, jump to FIB1
+    lb $t1, 0($t1)
+    bgtz $t1, MEMO      # else if (memo[n] > 0) return memo[n]
+    j REC
+
+ZERO:
+    li $v0, 0   # Return 0
     jr $ra
+
+FIB1:
+    li $v0, 1   # Return 1
+    jr $ra
+    
+MEMO:
+    lb $v0, 0($t1)  # Return memo[n]
+    jr $ra
+
+REC:
+# Fib(N-1):
+# store state:
+	addi $sp,$sp,-8
+	sw $ra,4($sp)
+	sw $a0,0($sp) 
+	
+# calculate $a0 = N-1, then call Fib(N-1):	
+	addi $a0,$a0,-1
+	jal fib
+	
+# preserve state:
+    lw	$ra,4($sp)
+	lw $a0,0($sp)
+	addi $sp,$sp,8
+	
+## Enter Fib(N-2)
+# store state: $v0, value of Fib(N-1)
+    addi $sp,$sp,-12
+	sw $ra,8($sp)
+	sw $a0,4($sp)
+    sw $v0,0($sp)   # Store value of fib(n-1) on the stack
+    
+# calculate $a0 = N-2, then call Fib(N-2):
+	addi $a0,$a0,-2
+	jal fib	
+	
+# preserve state: Fib(N-1)
+    lw $ra,8($sp) 
+	lw $a0,4($sp)
+    lw $v1,0($sp)
+	addi $sp,$sp,12
+    
+## operation: memo[n] = Fib(N-2)+Fib(N-1)
+    add $a1,$v0,$v1
+    move$v0, $a1
+	jr $ra
