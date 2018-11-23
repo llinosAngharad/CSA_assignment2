2fb497c8bc91a9479cbea4f40f08bfa9ae6f7c3f
Llinos <llins1@hotmail.com>
2018-11-23 14:01:47 +0000

    Update csa_assignment2.s

--- csa_assignment2.s
+++ csa_assignment2.s
@@ -32,12 +32,12 @@ JFIB:
     addi $sp, $sp, -4   # Adjust stack pointer
     sw $a0, 0($sp)      # Store input N on the stack
     
-    jal make_heap       # make array on the heap, returns end address of array on heap
+    jal make_heap       # Make array on the heap, returns base address of memo on heap
     
     lw $a0, 0($sp)      # Load input N from the heap into parameter $a0
-    move $a1, $v0       # Move base address of array on heap into parameter $a1
+    move $a1, $v0       # Move base address of memo on heap into parameter $a1
     mul $t1, $a0, $t9   # n*4
-    addi $a1, $a1, $t1  # Add
+    add $a1, $a1, $t1   # Move address to end address of memo on heap
     jal fib             # Input is valid. Call fib subroutine
     
 PRINT:
@@ -49,17 +49,35 @@ EXIT:
     li $v0, 10      # System call code 10: exit
     syscall
 
+##
+# Allocates space on the heap for the memo
+#
+# @param $a0: user input N
+# @param $a1: N+1 i.e the amount of memo entries needed
+#
+# @return $v0: base address of memo on heap 
+##
 make_heap:
-    # Allocate heap space for array:
+    # Allocate heap space for memo:
     li $v0, 9           # System call code 9: allocate heap space
     mul $a0, $a1, $t9   # Calculate the amount of heap space needed (n+1)*4
     syscall
-    move $s1, $v0       # Save base address of heap
+    move $s1, $v0       # Save base address of heap memo
     jr $ra
 
+
+##
+# Returns all fibonacci numbers from 0 to n
+#
+# @param $a0: user input N
+# @param $a1: end address of array on heap
+#
+# @return $v0: fibonacci number
+##
 fib:
-    addi $sp, $sp, -8
-    sw $ra, 0($sp)
-    sw $a0, 4($sp)
+    addi $sp, $sp, -12
+    sw $ra, 8($sp)
+    sw $a0, 4($sp)      # Parameter $a0
+    sw $a1, 0($sp)      # Parameter $a1
     move $v0, $a0
     jr $ra
